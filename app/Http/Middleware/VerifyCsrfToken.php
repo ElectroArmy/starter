980a975d8445cc86b009bc6639bcf77e79a775f9
yameste <yameste@yahoo.co.uk>
2017-04-07 17:01:08 +0100

    added 5.4 routes in to verify csrf middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,9 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'url' =>  '/checkout/charges/' , '/messages'
+        '/checkout/charges/',
+        '/messages',
+        'stripe/*'
 
     ];
 }
