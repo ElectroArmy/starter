4b8a1a792cec44b924b23b9ee572ce712b1d0656
yameste <yameste@yahoo.co.uk>
2017-04-07 14:47:15 +0100

    removed extra array in verify csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'url' => '/messages',
-        'url' => '/checkout/charges/'
+        'url' => '/messages', '/checkout/charges/'
+        
     ];
 }
