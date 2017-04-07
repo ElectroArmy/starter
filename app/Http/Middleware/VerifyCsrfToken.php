60cc5de98477c92d6ac7f84ccea456752d1f2a28
yameste <yameste@yahoo.co.uk>
2017-04-07 14:43:53 +0100

    updated url and added charges url

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'url' => '/messages'
+        'url' => '/messages',
+        'url' => '/checkout/charges/'
     ];
 }
