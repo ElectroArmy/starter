a4d0ea4411ddf3d867f44eb573d52b14953c10b2
yameste <yameste@yahoo.co.uk>
2017-04-07 16:57:55 +0100

    edde

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'url' =>  '/checkout/charges/'
+        'url' =>  '/checkout/charges/' , '/messages'
 
     ];
 }
